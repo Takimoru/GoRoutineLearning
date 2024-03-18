25f7ff3ea60a554fdf72e651ce212d4df60afd0e
unknown <nicolastz127@gmail.com>
2024-03-18 20:51:47 +0800

    belajar RWMutex ketika mutex dibutuhkan untuk read data agar tidak terjadi race condition

--- Mutex_test.go
+++ Mutex_test.go
@@ -27,3 +27,39 @@ func TestMutex(t *testing.T) {
 
 // mutex merupakan solusi ketika menghadapi RaceCondition dimana variable akan di lock ke 1 go routine lalu di unlock
 // seperti mengadakan antrian tetapi aman digunakan dan counter menunjukkan 100.000 sebagaimana aslinya
+
+type BankAccount struct {
+	RWMutex sync.RWMutex
+	Balance int
+}
+
+func (account *BankAccount) AddBalance(amount int) {
+	account.RWMutex.Lock()
+	account.Balance = account.Balance + amount
+	account.RWMutex.Unlock()
+}
+
+func (account *BankAccount) GetBalance() int {
+	account.RWMutex.RLock()
+	balance := account.Balance
+	account.RWMutex.RUnlock()
+	return balance
+}
+
+func TestRWMutex(t *testing.T) {
+	account := BankAccount{}
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			for j := 0; j < 100; j++ {
+				account.AddBalance(1)
+				fmt.Println(account.GetBalance())
+			}
+		}()
+
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Total Balance", account.GetBalance())
+
+}
